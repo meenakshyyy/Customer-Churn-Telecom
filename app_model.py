8c852b95b7df624062091c232d5d6abe78fd2a9e
Meenakshy <107634332+meenakshyyy@users.noreply.github.com>
2025-01-22 22:56:11 +0530

    Update app_model.py

--- app_model.py
+++ app_model.py
@@ -3,9 +3,7 @@ import pandas as pd
 import pickle
 
 # Load the Random Forest model from the pickle file
-model_file_path=model.pkl
-with open(model_file_path, 'rb') as model_file:
-    model = pickle.load(model_file)
+model = pickle.load(open('model.pkl', 'rb'))
 
 columns =["Gender", "SeniorCitizen","Partner","Dependents","PhoneService","MultipleLines", "InternetServiceType", "OnlineSecurity", "OnlineBackup", 
                          "DeviceProtection", "TechSupport", "StreamingTV", "StreamingMovies", "ContractType",  "PaperlessBilling",
